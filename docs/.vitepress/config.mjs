c72a63c6ba6eb559635afd3fab6ffe957c418842
luckyloo1730 <luckyloo1730@gmail.com>
2024-08-01 15:49:27 +0300

    Updated headers in description

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "SmileNodes Wiki",
-  description: "SmileNodes Wiki",
+  description: "База знаний для клиентов хостинга, доступна каждому!",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
